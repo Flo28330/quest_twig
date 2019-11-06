2f975239c097c5fb663571f4439a8b79a5c197ca
Flo28330 <florence.sivadier@gmail.com>
2019-11-06 23:54:13 +0100

    expédition de la quête : Twig

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../src/view');
+$twig = new \Twig\Environment($loader, [
+        'cache' => false
+    ]
+);
+
+
+$products = ['Carambar', 'Batnan', 'Chamallow', 'Ourson-guimauve', 'Chuppa chups', 'm&m\s'];
+
+
+echo $twig->render('index.html.twig', [
+    'bonbons' => $products
+]);
---
+++ vendor/twig/twig/src/Extension/TwigEnvironment.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class TwigEnvironment
+{
+
+    /**
+     * TwigEnvironment constructor.
+     * @param TwigLoaderFilesystemLoader $loader
+     */
+    public function __construct(\TwigLoaderFilesystemLoader $loader)
+    {
+    }
+}
